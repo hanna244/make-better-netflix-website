7d73d68b4f1b3bae1912fb16909b5d3d353b1dd5
hanna244 <shhn0509@gmail.com>
2020-11-28 21:16:23 +0900

    [실습] 헤더 디자인까지 완료

--- js/dev.js
+++ js/dev.js
@@ -1,6 +1,6 @@
-const colorSchene = {
-  red: "#E50914",
-  black: "#010101",
+const COLOR_SCHENE = {
+  red: '#E50914',
+  black: '#010101',
 }
 
 /* -------------------------------------------------------------------------- */
@@ -8,16 +8,14 @@ const colorSchene = {
 const { red, black } = COLOR_SCHENE
 
 console.log(
-  "%cMake Better! Netflix Website Homepage",
-  "color: white; font-size: 1rem; background: #E50914; padding: 0.45em 0.82em"
+  '%cMake Better! Netflix Website Homepage',
+  'color: white; font-size: 1rem; background: #E50914; padding: 0.45em 0.82em'
 )
 console.log(
-  "%cHanna244(hanna244@euid.dev)",
-  "color: white; font-size: 0.2rem; background: #111; padding: 1em 2em"
+  '%cHanna244(hanna244@euid.dev)',
+  'color: white; font-size: 0.2rem; background: #111; padding: 1em 2em'
 )
 /* -------------------------------------------------------------------------- */
 
-function dev() {
-  const hiddenContents = document.querySelectorAll("main footer")
-  hiddenContents.forEach((content) => content.setAttribute("hidden", "hidden"))
-}
+const hiddenContents = document.querySelectorAll('main, footer')
+hiddenContents.forEach((content) => content.setAttribute('hidden', 'hidden'))
