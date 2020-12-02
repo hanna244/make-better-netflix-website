7e15da07da200ab65d43e439c7a88bd7a94c5e16
hanna244 <shhn0509@gmail.com>
2020-12-02 15:15:54 +0900

    [실습] 인풋 자바스크립트 (확인 필요)

--- js/dev.js
+++ js/dev.js
@@ -15,9 +15,29 @@ const emailInput = emailContainer.querySelector('input')
 // emailInput.addEventListener('focus', () => console.log('focus'))
 // emailInput.addEventListener('blur', () => console.log('blur focus'))
 
-emailInput.addEventListener('focus', () =>
-  emailContainer.classList.add('promotion__emailContainer--focused')
+// emailInput.addEventListener('focus', () =>
+//   emailContainer.classList.add('promotion__emailContainer--focused')
+// )
+// emailInput.addEventListener('blur', () =>
+//   emailContainer.classList.remove('promotion__emailContainer--focused')
+// )
+
+/* 사용자의 입력을 검토(감지)하여 유효 또는 무효한 상태를 감지하는 스크립팅 --------------------- */
+const promotionEmailContainerNode = document.querySelector(
+  '.promotion__emailContainer'
 )
-emailInput.addEventListener('blur', () =>
-  emailContainer.classList.remove('promotion__emailContainer--focused')
+const emailInputNode = promotionEmailContainerNode.querySelector(
+  'input[type="email"]'
 )
+const INVALID_CLASS = '.promotion__emailContainer--invalid'
+
+// change 이벤트는 텍스트를 입력하고 커서가 인풋에서 나와야지만 동작이 감지된다. (별로 좋지 않음, 바로 인식하기를 바람)
+// input 텍스트를 입력 할 때마다 동작을 감지한다.
+// invalid 전송할 때 이벤트 동작 감지
+// matches() 메서드를 사용해서 엘리먼트의 입력한 값이 옳은지 확인.
+emailInputNode.addEventListener('input', (e) => {
+  const isInvalid = e.target.matches(':invalid')
+  isInvalid
+    ? promotionEmailContainerNode.classList.add(INVALID_CLASS)
+    : promotionEmailContainerNode.classList.remove(INVALID_CLASS)
+})
