7713714c3e6e706cf0ff84d2baed79fd237a7f3e
hanna244 <shhn0509@gmail.com>
2020-12-12 08:47:16 +0900

    [파일 수정] 속성 순서 변경

--- utils/hiddenHTMLElements.js
+++ utils/hiddenHTMLElements.js
@@ -1,6 +1,16 @@
+/* 함수 값이 없을 경우 console에 오류가 떠서 수정 */
 function hiddenHTMLlements(selector) {
+  if (!selector) return
   const hiddenContents = document.querySelectorAll(selector)
-  hiddenContents.forEach((selector) =>
-    selector.setAttribute('hidden', 'hidden')
+  hiddenContents.forEach(
+    (content) => (content.style.cssText = 'display: none !important')
   )
 }
+
+/* 함수 값이 있을 때(공백 X) */
+// function hiddenHTMLlements(selector) {
+//   const hiddenContents = document.querySelectorAll(selector)
+//   hiddenContents.forEach((selector) =>
+//     selector.setAttribute('hidden', 'hidden')
+//   )
+// }
