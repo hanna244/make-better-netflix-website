ca5f08e9e0047953bafeab84dfeabdd9fc6204d3
hanna244 <shhn0509@gmail.com>
2021-01-19 23:16:29 +0900

    [개선] detectScrollPosition: 하나의 기능만 하는 show, hide함수

--- src/js/detectScrollPosition.js
+++ src/js/detectScrollPosition.js
@@ -1,24 +1,48 @@
 const SCROLL_SHOW_STATE_CLASSNAME = 'scrolling--show'
 const fixedHeaderNode = document.querySelector('.header--fixed')
 
-const scrollShowHide = function (target, Ypos = 0) {
-  if (
-    window.scrollY >= Ypos &&
-    !target.classList.contains(SCROLL_SHOW_STATE_CLASSNAME)
-  ) {
-    target.classList.add(SCROLL_SHOW_STATE_CLASSNAME)
+// 헬퍼 함수
+var scrollShow = function (target, Ypos = 0, className) {
+  if (window.scrollY < Ypos && !target.classList.contains(className)) {
+    target.classList.add(className)
   }
+}
 
-  if (
-    window.scrollY < Ypos &&
-    target.classList.contains(SCROLL_SHOW_STATE_CLASSNAME)
-  ) {
-    target.classList.remove(SCROLL_SHOW_STATE_CLASSNAME)
+const scrollHide = function (target, Ypos = 0, className) {
+  if (window.scrollY >= Ypos && target.classList.contains(className)) {
+    target.classList.remove(className)
   }
 }
 
 const handleScrollShowHide = function () {
-  scrollShowHide(fixedHeaderNode, 30)
+  scrollShow(fixedHeaderNode, 30, SCROLL_SHOW_STATE_CLASSNAME)
+  scrollHide(fixedHeaderNode, 30, SCROLL_SHOW_STATE_CLASSNAME)
 }
 
 window.addEventListener('scroll', handleScrollShowHide)
+
+// const scrollShowHide = function (target, Ypos = 0) {
+//   if (window.scrollY >= Ypos && !hasShowClass) {
+//     addClass(target, SCROLL_SHOW_STATE_CLASSNAME)
+//   }
+
+//   if (SY < Ypos && hasShowClass) {
+//     removeClass(target, SCROLL_SHOW_STATE_CLASSNAME)
+//   }
+// }
+
+// const scrollShowHide = function (target, Ypos = 0) {
+//   if (
+//     window.scrollY >= Ypos &&
+//     !target.classList.contains(SCROLL_SHOW_STATE_CLASSNAME)
+//   ) {
+//     target.classList.add(SCROLL_SHOW_STATE_CLASSNAME)
+//   }
+
+//   if (
+//     window.scrollY < Ypos &&
+//     target.classList.contains(SCROLL_SHOW_STATE_CLASSNAME)
+//   ) {
+//     target.classList.remove(SCROLL_SHOW_STATE_CLASSNAME)
+//   }
+// }
