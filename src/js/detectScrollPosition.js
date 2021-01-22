a93457e1052196b0e08182d77d4833a79cb4c00f
hanna244 <shhn0509@gmail.com>
2021-01-22 23:55:27 +0900

    [개선] 코드 리팩토링 후 주석 제거

--- src/js/detectScrollPosition.js
+++ src/js/detectScrollPosition.js
@@ -3,13 +3,13 @@ const fixedHeaderNode = document.querySelector('.header--fixed')
 
 // 헬퍼 함수
 var scrollShow = function (target, Ypos = 0, className) {
-  if (window.scrollY < Ypos && !target.classList.contains(className)) {
+  if (window.scrollY >= Ypos && !target.classList.contains(className)) {
     target.classList.add(className)
   }
 }
 
 const scrollHide = function (target, Ypos = 0, className) {
-  if (window.scrollY >= Ypos && target.classList.contains(className)) {
+  if (window.scrollY < Ypos && target.classList.contains(className)) {
     target.classList.remove(className)
   }
 }
@@ -20,29 +20,3 @@ const handleScrollShowHide = function () {
 }
 
 window.addEventListener('scroll', handleScrollShowHide)
-
-// const scrollShowHide = function (target, Ypos = 0) {
-//   if (window.scrollY >= Ypos && !hasShowClass) {
-//     addClass(target, SCROLL_SHOW_STATE_CLASSNAME)
-//   }
-
-//   if (SY < Ypos && hasShowClass) {
-//     removeClass(target, SCROLL_SHOW_STATE_CLASSNAME)
-//   }
-// }
-
-// const scrollShowHide = function (target, Ypos = 0) {
-//   if (
-//     window.scrollY >= Ypos &&
-//     !target.classList.contains(SCROLL_SHOW_STATE_CLASSNAME)
-//   ) {
-//     target.classList.add(SCROLL_SHOW_STATE_CLASSNAME)
-//   }
-
-//   if (
-//     window.scrollY < Ypos &&
-//     target.classList.contains(SCROLL_SHOW_STATE_CLASSNAME)
-//   ) {
-//     target.classList.remove(SCROLL_SHOW_STATE_CLASSNAME)
-//   }
-// }
